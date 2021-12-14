ee8ae04199893272713ce5b6c6d7f6278012d2cf
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2021-12-14 07:36:57 +0200

    Create Make It Good.cpp

---
+++ Make It Good.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+using namespace std;
+void CodeFast (){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+#endif
+}
+
+int main(){
+    CodeFast();
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        vector<int>nums(n);
+        for (auto &i :nums)
+            cin >> i;
+
+        int i = n - 1;
+        while (i > 0){
+            if (nums[i] < nums[i - 1])
+                i--;
+            else
+                break;
+        }
+        while (i > 0){
+            if (nums[i] >= nums[i - 1])
+                i--;
+            else
+                break;
+        }
+        cout << i << nl;
+        for(int l  = 0;l < i;++l )
+            cout << nums[l] <<' ';
+        cout << nl;
+    }
+    return 0;
+}
