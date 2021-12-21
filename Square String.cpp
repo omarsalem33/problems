f31bf92e9680fe0060e38e12df88c642250f8775
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2021-12-21 07:37:02 +0200

    Create Square String.cpp

---
+++ Square String.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+using namespace std;
+void CodeFast (){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+#endif
+}
+
+
+int main(){
+    CodeFast();
+   int t; cin>>t;
+   while(t--){
+       ll n; cin >> n;
+       set<ll>s;
+       for (int i = 1; i*i <=n ; ++i) {
+           s.insert(i*i);
+       }
+       for (int i = 1; i*i* i<=n ; ++i) {
+           s.insert(i*i*i);
+       }
+       cout << s.size()<<nl;
+   }
+    return 0;
+}
